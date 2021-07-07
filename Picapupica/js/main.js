e4e38510a7865274aef7cdf28b0fef545e920012
aYTRo <69348389+aYTRo@users.noreply.github.com>
2021-07-07 18:20:56 +0300

    Delete Picapupica directory

--- Picapupica/js/main.js
+++
@@ -1,7 +0,0 @@
-let menuToggle = document.querySelector('#menu_toggle');
-let menu = document.querySelector('.sidebar')
-
-menuToggle.addEventListener('click', function (event) {
-  event.preventDefault();
-  menu.classList.toggle('visible');
-})
